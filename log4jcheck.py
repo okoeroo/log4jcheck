b02bc8b8f88e84255069d2964ec2e8342a0cea2a
Oscar Koeroo <okoeroo@gmail.com>
2021-12-12 22:40:09 +0000

    Added a timeout

--- log4jcheck.py
+++ log4jcheck.py
@@ -28,11 +28,17 @@ def argparsing(exec_file):
                         help="Target host to examine",
                         default=None,
                         type=str)
+    parser.add_argument("--timeout",
+                        dest='timeout',
+                        help="Timeout",
+                        default=15,
+                        type=int)
+
 
     args = parser.parse_args()
     return args
 
-def check1(schema, url_input, reply_host, header_name, payl):
+def check1(schema, url_input, reply_host, header_name, payl, timeout):
     logging.info(f"check1: Sending request to {schema}{url_input.hostname} using {header_name} injecting {payl}")
 
     # Check 1 (User Agent)
@@ -40,33 +46,35 @@ def check1(schema, url_input, reply_host, header_name, payl):
         requests.get(
             f"{schema}{url_input.hostname}",
             headers={header_name: payl},
-            verify=False
+            verify=False,
+            timeout=timeout
         )
     except requests.exceptions.ConnectionError as e:
         logging.error(f"HTTP connection to {schema}{url_input.hostname} URL error: {e}")
 
-def check2(schema, url_input, reply_host, payl):
+def check2(schema, url_input, reply_host, payl, timeout):
     logging.info(f"check2: Sending request to {schema}{url_input.hostname} injecting {payl}")
 
     # Check 2 (Get request)
     try:
         requests.get(
             f"{schema}{url_input.hostname}/{payl}",
-            verify=False
+            verify=False,
+            timeout=timeout
         )
     except requests.exceptions.ConnectionError as e:
         logging.error(f"HTTP connection to {schema}{url_input.hostname} URL error: {e}")
 
 
-def deliver(url_input, reply_host, header_name, payl):
+def deliver(url_input, reply_host, header_name, payl, timeout):
     schema = ['http://', 'https://']
 
     for s in schema:
-        check1(s, url_input, reply_host, header_name, payl)
-        check2(s, url_input, reply_host, payl)
+        check1(s, url_input, reply_host, header_name, payl, timeout)
+        check2(s, url_input, reply_host, payl, timeout)
 
 
-def payload_generation(url_input, reply_host):
+def payload_generation(url_input, reply_host, timeout):
     identifier = uuid.uuid4()
     logging.debug(f"Generated UUID: {identifier}")
 
@@ -78,7 +86,7 @@ def payload_generation(url_input, reply_host):
     header_name = 'User-Agent'
 
     for payl in payloads:
-        deliver(url_input, reply_host, header_name, payl)
+        deliver(url_input, reply_host, header_name, payl, timeout)
 
 
 def main():
@@ -91,7 +99,7 @@ def main():
         url_input = urlparse('dummy://' + args.target)
 
     # Generate payload and run it
-    payload_generation(url_input, args.replyfqdn)
+    payload_generation(url_input, args.replyfqdn, args.timeout)
 
 
 if __name__ == "__main__":
